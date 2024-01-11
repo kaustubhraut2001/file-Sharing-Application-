63a654d3e6881f26474bf7e5401b8210ae163b05
Kaustubh <kaustubhr2001@gmail.com>
2024-01-12 00:38:27 +0530

    functions created to create zip and handle uplodad the file

--- filesharing/home/serilizers.py
+++ filesharing/home/serilizers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Folder
+import shutil
 
 
 
@@ -7,11 +8,17 @@ from .models import Folder
 class FileListSerilizers(serializers.Serializer):
 	files = serializers.ListField(child=serializers.FileField(max_length=100000, allow_empty_file=False, use_url=False))
 
+
+	#  create zip function
+	def createzip(self , folder):
+		shutil.make_archive(str(folder) , 'zip' , 'public/static/{folder}')
+
 	def create(self , validate_data):
 		folder = Folder.objects.create()
 		files = validate_data.pop('files')
 		for file in files:
 			file_obj = folder.files.create(folder = folder , file=file)
 			file_obj.append(file_obj)
+		self.createzip(folder.uuid)
 
-		return file_obj
+		return {'files' : {} , folder : str(Folder.uuid)}
--- filesharing/home/views.py
+++ filesharing/home/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 
+from rest_framework.response import Response
+
+from rest_framework import status
+
+from .serializers import FileListSerilizers
 # Create your views here.
+
+class handlefileupload(APIView):
+
+	def post(self, request, format=None):
+		try:
+			serializer = FileListSerilizers(data=request.data)
+			if serializer.is_valid():
+				serializer.save()
+				return Response(serializer.data, status=status.HTTP_201_CREATED)
+			else:
+				return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+		except Exception as e:
+			return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
